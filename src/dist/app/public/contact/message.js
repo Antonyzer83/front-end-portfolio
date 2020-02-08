370889a9a1d4e570d8ee3cceb9d04b784ad095eb
Antony CASTANER <antony.castanerpynov.com>
2020-02-08 17:06:00 +0100

    Update files

---
+++ src/dist/app/public/contact/message.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Message {
+    constructor(first_name, last_name, email, subject, content) {
+    }
+}
+exports.Message = Message;
+//# sourceMappingURL=message.js.map
